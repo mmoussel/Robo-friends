11b48c3aaef324cede8fc5d3131607c74df1ddef
mmoussel <moussel2011@gmail.com>
2021-03-29 14:35:46 +0200

    applying hooks

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,4 +1,55 @@
-import React, { Component } from "react";
+// import React, { Component } from "react";
+// import { connect } from "react-redux";
+// import CardList from "../components/CardList";
+// import SearchBox from "../components/SearchBox";
+// import "./App.css";
+// import Scroll from "../components/Scroll";
+// import ErrorBoundray from "../components/ErroreBoundary";
+// import { setSearchField, requestRobots } from "../actions";
+
+// const mapStateToProps = (state) => {
+//   return {
+//     searchField: state.searchRobots.searchField,
+//     isPending: state.requestRobots.isPending,
+//     robots: state.requestRobots.robots,
+//     error: state.requestRobots.error,
+//   };
+// };
+// const mapDispatchToProps = (dispatch) => {
+//   return {
+//     onSearchChange: (event) => dispatch(setSearchField(event.target.value)),
+//     onRequestChange: () => dispatch(requestRobots()),
+//   };
+// };
+
+// class App extends Component {
+//   componentDidMount() {
+//     this.props.onRequestChange();
+//   }
+
+//   render() {
+//     // console.log('render')
+//     const { searchField, onSearchChange, robots } = this.props;
+//     const filterdRobots = robots.filter((robot) => {
+//       return robot.name.toLowerCase().includes(searchField.toLowerCase());
+//     });
+//     return !robots.length ? (
+//       <h1 className="tc f1">loading</h1>
+//     ) : (
+//       <div className="tc">
+//         <h1 className="f1">Robo Friends</h1>
+//         <SearchBox searchChange={onSearchChange} />
+//         <ErrorBoundray>
+//           <Scroll>
+//             <CardList robots={filterdRobots} />
+//           </Scroll>
+//         </ErrorBoundray>
+//       </div>
+//     );
+//   }
+// }
+// export default connect(mapStateToProps, mapDispatchToProps)(App);
+import React, { useEffect } from "react";
 import { connect } from "react-redux";
 import CardList from "../components/CardList";
 import SearchBox from "../components/SearchBox";
@@ -22,30 +73,32 @@ const mapDispatchToProps = (dispatch) => {
   };
 };
 
-class App extends Component {
-  componentDidMount() {
-   this.props.onRequestChange();
-  }
+function App(props) {
+  //   componentDidMount() {
+  //     this.props.onRequestChange();
+  //   }
+  const { searchField, onSearchChange, robots, onRequestChange } = props;
+  useEffect(() => {
+    // Update the document title using the browser API
+    onRequestChange();
+  }, [onRequestChange]);
 
-  render() {
-    // console.log('render')
-    const { searchField, onSearchChange , robots } = this.props;
-    const filterdRobots = robots.filter((robot) => {
-      return robot.name.toLowerCase().includes(searchField.toLowerCase());
-    });
-    return !robots.length ? (
-      <h1 className="tc f1">loading</h1>
-    ) : (
-      <div className="tc">
-        <h1 className="f1">Robo Friends</h1>
-        <SearchBox searchChange={onSearchChange} />
-        <ErrorBoundray>
-          <Scroll>
-            <CardList robots={filterdRobots} />
-          </Scroll>
-        </ErrorBoundray>
-      </div>
-    );
-  }
+  const filterdRobots = robots.filter((robot) => {
+    return robot.name.toLowerCase().includes(searchField.toLowerCase());
+  });
+
+  return !robots.length ? (
+    <h1 className="tc f1">loading</h1>
+  ) : (
+    <div className="tc">
+      <h1 className="f1">Robo Friends</h1>
+      <SearchBox searchChange={onSearchChange} />
+      <ErrorBoundray>
+        <Scroll>
+          <CardList robots={filterdRobots} />
+        </Scroll>
+      </ErrorBoundray>
+    </div>
+  );
 }
 export default connect(mapStateToProps, mapDispatchToProps)(App);
